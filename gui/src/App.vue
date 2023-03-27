b66f366ae31c77068a08fbabd2b9c3b28f83d86d
Zweleth <zwebeko@gmail.com>
2023-03-27 16:44:41 +0200

    a

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <navbar></navbar>
   <router-view/>
 </template>
+<script> 
+import navbar from '@/components/Navbar.vue'
+export default {
+  components: {
+    navbar
+  }
+}
+</script>
 
 <style>
 #app {
@@ -12,19 +17,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ gui/src/components/Navbar.vue
@@ -0,0 +1,87 @@
+<template lang="">
+  <header>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"
+          ><i class="fa-brands fa-bitcoin"></i>Coinplus</a
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+                <router-link to="/home">Home</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/signin">Sign in</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/signup">Sign up</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/about">About us</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+    name: 'navbar'
+};
+</script>
+<style scoped>
+header {
+    position: fixed;
+    width: 100vw;
+    z-index: +5;
+}
+
+.navbar {
+    color: gray;
+    background-color: #222;
+}
+
+.navbar a {
+    color: gray;
+    text-decoration: none;
+}
+.navbar-nav a.router-link-exact-active {
+    color: aqua;
+  }
+
+.navbar a:hover {
+    animation: linkHover 0.4s ease-in-out forwards;
+}
+
+.navbar-nav {
+    margin-left: auto;
+    margin-right: 2rem;
+    gap: 1.5rem;
+}
+
+.navbar-brand {
+    padding-left: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.fa-bitcoin {
+    margin-right: 0.4rem;
+    display: block;
+    scale: 1.2;
+}
+
+</style>
